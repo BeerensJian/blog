6910bef1800ce72e79f6eb32e24736f32e0a1b38
Jian Beerens <Beerensjian@gmail.com>
2023-10-22 22:45:53 +0200

    fixed link to author posts view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
                         {{ $post->title }}
                     </a>
                 </h2>
-                <p>Post by <a href="#">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+                <p>Post by <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
                 <p>{{ $post->excerpt }}</p>
             </article>
         @endforeach
