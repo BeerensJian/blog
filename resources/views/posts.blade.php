6fbac1aab3f455a842e254255334697c2d3a2fa9
Jian Beerens <beerensjian@gmail.com>
2023-10-23 19:36:05 +0200

    Illustrations & layout index page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,20 @@
 <x-layout>
-    <x-slot name="content">
-        @foreach($posts as $post)
-            <article>
-                <h2>
-                    <a href="/posts/{{ $post->id }}">
-                        {{ $post->title }}
-                    </a>
-                </h2>
-                <p>Post by <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-                <p>{{ $post->excerpt }}</p>
-            </article>
-        @endforeach
-    </x-slot>
+
+    @include('_posts-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-featured-post-card/>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+    </main>
 </x-layout>
+
 
