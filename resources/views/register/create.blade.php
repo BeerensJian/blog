d9473bb6d9b82c9cfe08267857b63af312c28587
Jian Beerens <jian.beerens@numble.be>
2023-10-28 12:55:03 +0200

    WIP register page

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,44 @@
+<x-layout>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <h1 class="text-center text-bold text-2xl">Hello world</h1>
+        <section class="max-w-xl mx-auto bg-gray-100 p-8">
+            <form action="/register" method="post">
+                @csrf
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                        Full Name
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                           id="name" type="text" placeholder="Name"
+                    >
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                           id="username" type="text" placeholder="Username"
+                    >
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                        Email
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                           id="email" type="email" placeholder="Email"
+                    >
+                </div>
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                           id="password" type="password" placeholder="*******"
+                    >
+                </div>
+                <button type="submit">Register</button>
+            </form>
+
+        </section>
+    </main>
+</x-layout>
